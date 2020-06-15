b2e67bfc5226ed2148b4701a05c381b8582294cb
Zijian Zhang <39160610+bulffi@users.noreply.github.com>
2020-06-15 16:27:54 +0800

    add a percentage

--- src/pages/portal/Manage.js
+++ src/pages/portal/Manage.js
@@ -1,19 +1,23 @@
-import React from 'react'
-import {Row, Col, Typography, Card, List, Button, notification} from 'antd'
+import React from 'react';
+import { Row, Col, Typography, Card, List, Button, notification } from 'antd';
 import { connect } from 'umi';
-const {Title} = Typography
-import BraftEditor from 'braft-editor'
-import 'braft-editor/dist/index.css'
-import 'braft-editor/dist/output.css'
+const { Title } = Typography;
+import BraftEditor from 'braft-editor';
+import 'braft-editor/dist/index.css';
+import 'braft-editor/dist/output.css';
 import Get from '../../util/Get';
 import { baseUrl } from '../../util/ConstStore';
 
-
-
-
-const mapStateToProps = (state) => {
-  const { commentList,fatherContent, noMoreComment,pageSize, lastId, lastHost  } = state['AUDIT']
-  const { userId } = state['USER']
+const mapStateToProps = state => {
+  const {
+    commentList,
+    fatherContent,
+    noMoreComment,
+    pageSize,
+    lastId,
+    lastHost,
+  } = state['AUDIT'];
+  const { userId } = state['USER'];
   return {
     commentList,
     noMoreComment,
@@ -23,10 +27,10 @@ const mapStateToProps = (state) => {
     fatherContent,
     userId,
     loading: state.loading.models.AUDIT,
-  }
-}
+  };
+};
 
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = dispatch => {
   return {
     onInit: (pageSize, lastId, lastHost, userId) => {
       const action = {
@@ -36,37 +40,37 @@ const mapDispatchToProps = (dispatch) => {
           lastId,
           lastHost,
           userId,
-        }
-      }
-      dispatch(action)
+        },
+      };
+      dispatch(action);
     },
     onDestroy: () => {
       const action = {
         type: 'AUDIT/resetCommentList',
-        payload: {}
-      }
-      dispatch(action)
+        payload: {},
+      };
+      dispatch(action);
     },
-    onPass: (comment) => {
+    onPass: comment => {
       const action = {
         type: 'AUDIT/passComment',
         payload: {
-          comment
-        }
-      }
-      dispatch(action)
+          comment,
+        },
+      };
+      dispatch(action);
     },
     onDeny: (hostPage, commentId) => {
       const action = {
         type: 'AUDIT/denyComment',
         payload: {
           hostPage,
-          commentId
-        }
-      }
-      dispatch(action)
+          commentId,
+        },
+      };
+      dispatch(action);
     },
-    onMoreComment: (pageSize, lastId, lastHost,userId) => {
+    onMoreComment: (pageSize, lastId, lastHost, userId) => {
       const action = {
         type: 'AUDIT/getCommentList',
         payload: {
@@ -74,123 +78,197 @@ const mapDispatchToProps = (dispatch) => {
           lastId,
           lastHost,
           userId,
-        }
-      }
-      dispatch(action)
+        },
+      };
+      dispatch(action);
     },
-    onChangeFatherContent: (content) => {
+    onChangeFatherContent: content => {
       const action = {
         type: 'AUDIT/setFatherContent',
         payload: {
-          content
-        }
-      }
-    }
-  }
-}
+          content,
+        },
+      };
+    },
+  };
+};
 
 @connect(mapStateToProps, mapDispatchToProps)
 export default class Manage extends React.Component {
   state = {
-    fatherLoading: false
-  }
+    fatherLoading: false,
+    rate: 0,
+  };
 
   componentDidMount() {
-    this.props.onInit(this.props.pageSize, this.props.lastId, this.props.lastHost, this.props.userId)
+    this.props.onInit(
+      this.props.pageSize,
+      this.props.lastId,
+      this.props.lastHost,
+      this.props.userId,
+    );
+    const getPassStatURL = baseUrl + 'api/query';
+    Get(getPassStatURL).then(data => {
+      console.log(data);
+      if (data.code === 200) {
+        this.setState({ ...this.state, rate: data.rate });
+      }
+    });
   }
   componentWillUnmount() {
-    this.props.onDestroy()
+    this.props.onDestroy();
   }
 
-  passComment = (comment) => {
-    console.log(comment)
-    const lengthBefore = this.props.commentList.length
-    this.props.onPass(comment)
+  passComment = comment => {
+    console.log(comment);
+    const lengthBefore = this.props.commentList.length;
+    this.props.onPass(comment);
     if (lengthBefore === 1) {
-      console.log('need more')
-      this.props.onMoreComment(this.props.pageSize, this.props.lastId, this.props.lastHost, this.props.userId)
+      console.log('need more');
+      this.props.onMoreComment(
+        this.props.pageSize,
+        this.props.lastId,
+        this.props.lastHost,
+        this.props.userId,
+      );
     }
-  }
+  };
 
-  denyComment = (comment) => {
-    console.log(comment)
-    const lengthBefore = this.props.commentList.length
-    this.props.onDeny(comment.hostPage, comment.commentId)
+  denyComment = comment => {
+    console.log(comment);
+    const lengthBefore = this.props.commentList.length;
+    this.props.onDeny(comment.hostPage, comment.commentId);
     if (lengthBefore === 1) {
-      this.props.onMoreComment(this.props.pageSize, this.props.lastId, this.props.lastHost, this.props.userId)
+      this.props.onMoreComment(
+        this.props.pageSize,
+        this.props.lastId,
+        this.props.lastHost,
+        this.props.userId,
+      );
     }
-  }
+  };
 
-  getURLFromId = (host) => {
-    let sharpId = host.indexOf('#')
-    return host.substr(sharpId+ 1, host.length - sharpId)
-  }
+  getURLFromId = host => {
+    let sharpId = host.indexOf('#');
+    return host.substr(sharpId + 1, host.length - sharpId);
+  };
 
   getFatherContent = async (hostPage, commentId) => {
-    const getCommentURL = baseUrl + 'api/comment'
-    const data = await Get(getCommentURL, {params:{hostPage, commentId}})
+    const getCommentURL = baseUrl + 'api/comment';
+    const data = await Get(getCommentURL, { params: { hostPage, commentId } });
     // console.log(data)
     // this.props.onChangeFatherContent(data.content)
-    this.setState({...this.state, fatherLoading: false})
-    return data.content
-  }
+    this.setState({ ...this.state, fatherLoading: false });
+    return data.content;
+  };
 
-  onViewFather = async (comment) => {
-    this.setState({...this.state, fatherLoading: true})
-    const content = await this.getFatherContent(comment.hostPage, comment.fatherID)
-    const contentObj = JSON.parse(content)
-    console.log(contentObj)
+  onViewFather = async comment => {
+    this.setState({ ...this.state, fatherLoading: true });
+    const content = await this.getFatherContent(
+      comment.hostPage,
+      comment.fatherID,
+    );
+    const contentObj = JSON.parse(content);
+    console.log(contentObj);
     notification.open({
       message: 'Father comment',
       description: contentObj.blocks[0].text,
-      duration: 5
-    })
-  }
+      duration: 5,
+    });
+  };
 
-  getCardExtra = (comment) => {
-    const viewSource = (<a key={'0'} target="_blank" href={this.getURLFromId(comment.hostPage)}>{this.getURLFromId(comment.hostPage)}</a>)
-    let slashIndex = comment.commentId.indexOf('#')
-    let defineChar = comment.commentId.charAt(slashIndex + 1)
+  getCardExtra = comment => {
+    const viewSource = (
+      <a key={'0'} target="_blank" href={this.getURLFromId(comment.hostPage)}>
+        {this.getURLFromId(comment.hostPage)}
+      </a>
+    );
+    let slashIndex = comment.commentId.indexOf('#');
+    let defineChar = comment.commentId.charAt(slashIndex + 1);
     if (defineChar === 'S') {
-      const viewFather = (<div key={'1'}><Button loading={this.state.fatherLoading} onClick={()=>this.onViewFather(comment)} type={'dashed'}>View Father</Button></div>)
-      return [viewFather, viewSource]
+      const viewFather = (
+        <div key={'1'}>
+          <Button
+            loading={this.state.fatherLoading}
+            onClick={() => this.onViewFather(comment)}
+            type={'dashed'}
+          >
+            View Father
+          </Button>
+        </div>
+      );
+      return [viewFather, viewSource];
     }
-    return[viewSource]
-    }
+    return [viewSource];
+  };
 
   render() {
-    return(
-      <div style={{paddingTop: '50px'}}>
-        <Title style={{fontFamily:'monospace', fontSize: '30px', textAlign: 'center'}}>Comments To Audit</Title>
+    return (
+      <div style={{ paddingTop: '50px' }}>
+        <Title
+          style={{
+            fontFamily: 'monospace',
+            fontSize: '30px',
+            textAlign: 'center',
+          }}
+        >
+          Comments To Audit [History pass rate: {this.state.rate * 100} %]
+        </Title>
         <Row>
-          <Col span={3}/>
+          <Col span={3} />
           <Col span={18}>
             <List
               loading={this.props.loading}
               dataSource={this.props.commentList}
               renderItem={item => (
-                <div style={{paddingBottom: '20px'}}>
-                  <Card title={item.authorName + '  [' + item.email + ']'}  extra={this.getCardExtra(item)} actions={
-                    [
-                      <div><Button size={'small'} type={'primary'} onClick={()=>this.passComment(item)}>OK</Button></div>,
-                      <div><Button size={'small'} type={'primary'} danger={true} onClick={()=>this.denyComment(item)}>Deny</Button> </div>
-                    ]
-                  }>
-                    <div className="braft-output-content" dangerouslySetInnerHTML={{__html: BraftEditor.createEditorState(item.content).toHTML()}}/>
+                <div style={{ paddingBottom: '20px' }}>
+                  <Card
+                    title={item.authorName + '  [' + item.email + ']'}
+                    extra={this.getCardExtra(item)}
+                    actions={[
+                      <div>
+                        <Button
+                          size={'small'}
+                          type={'primary'}
+                          onClick={() => this.passComment(item)}
+                        >
+                          OK
+                        </Button>
+                      </div>,
+                      <div>
+                        <Button
+                          size={'small'}
+                          type={'primary'}
+                          danger={true}
+                          onClick={() => this.denyComment(item)}
+                        >
+                          Deny
+                        </Button>{' '}
+                      </div>,
+                    ]}
+                  >
+                    <div
+                      className="braft-output-content"
+                      dangerouslySetInnerHTML={{
+                        __html: BraftEditor.createEditorState(
+                          item.content,
+                        ).toHTML(),
+                      }}
+                    />
                   </Card>
                 </div>
               )}
-              />
+            />
             {/*{this.state.commentToJudge.map(((value, index) => {*/}
             {/*    return(*/}
             {/*      */}
             {/*    )*/}
             {/*}))}*/}
           </Col>
-          <Col span={3}/>
+          <Col span={3} />
         </Row>
-        <div style={{height: '100px'}}/>
+        <div style={{ height: '100px' }} />
       </div>
-    )
+    );
   }
 }
--- src/pages/portal/Stat.js
+++ src/pages/portal/Stat.js
@@ -1,24 +1,21 @@
-import React from 'react'
-import {Row, Col, Typography, Card, Space, message} from 'antd'
-import axios from 'axios'
+import React from 'react';
+import { Row, Col, Typography, Card, Space, message } from 'antd';
+import axios from 'axios';
 import { baseUrl } from '../../util/ConstStore';
 import Get from '../../util/Get';
 import { connect } from 'umi';
-const {Title} = Typography
+const { Title } = Typography;
 
-
-
-const mapStateToProps = (state) => {
-  const { userId } = state['USER']
+const mapStateToProps = state => {
+  const { userId } = state['USER'];
   return {
-    userId
-  }
-}
+    userId,
+  };
+};
 
-
-const mapDispatchToProps = (dispatch) => {
-  return {}
-}
+const mapDispatchToProps = dispatch => {
+  return {};
+};
 
 @connect(mapStateToProps, mapDispatchToProps)
 export default class Stat extends React.Component {
@@ -26,70 +23,113 @@ export default class Stat extends React.Component {
     balance: 0,
     writeRequest: 0,
     readRequest: 0,
-    loading: true
-  }
+    loading: true,
+  };
 
   componentDidMount() {
-    const getStatURL = baseUrl + 'api/stat'
-    const data = Get(getStatURL).then((data) => {
+    const getStatURL = baseUrl + 'api/stat';
+    const data = Get(getStatURL).then(data => {
       if (data.code === 200) {
-        message.success('Get stat success')
-        const {balance, readCount, writeCount} = data.userStat
+        message.success('Get stat success');
+        const { balance, readCount, writeCount } = data.userStat;
+        console.log(readCount, writeCount);
         this.setState({
           ...this.state,
           balance: balance,
-          writeRequest: readCount,
-          readRequest: writeCount,
-          loading: false
-        })
+          writeRequest: writeCount,
+          readRequest: readCount,
+          loading: false,
+        });
       } else {
-        message.error('Get stat fail')
+        message.error('Get stat fail');
         this.setState({
           ...this.state,
-          loading: false
-        })
+          loading: false,
+        });
       }
-    })
+    });
   }
 
   render() {
-    return(
-      <div style={{paddingTop: '50px', paddingBottom: '100px'}}>
-        <Title level={1} style={{fontFamily: 'monospace', textAlign: 'center'}}>Recent Stats</Title>
+    return (
+      <div style={{ paddingTop: '50px', paddingBottom: '100px' }}>
+        <Title
+          level={1}
+          style={{ fontFamily: 'monospace', textAlign: 'center' }}
+        >
+          Recent Stats
+        </Title>
         <Row>
-          <Col span={2}/>
+          <Col span={2} />
           <Col span={6}>
-            <Card loading={this.state.loading} title="Balance" >
+            <Card loading={this.state.loading} title="Balance">
               <Space direction={'horizontal'}>
-                <div style={{fontFamily:'monospace', fontSize: '80px'}}>$</div>
-                <div style={{fontFamily: 'monospace', fontSize: '25px', paddingTop: '40px'}}>{this.state.balance}</div>
+                <div style={{ fontFamily: 'monospace', fontSize: '80px' }}>
+                  $
+                </div>
+                <div
+                  style={{
+                    fontFamily: 'monospace',
+                    fontSize: '25px',
+                    paddingTop: '40px',
+                  }}
+                >
+                  {this.state.balance}
+                </div>
               </Space>
             </Card>
           </Col>
-          <Col span={1}/>
+          <Col span={1} />
           <Col span={6}>
-            <Card loading={this.state.loading} title="Total View" style={{ width: 300 }}>
+            <Card
+              loading={this.state.loading}
+              title="Total View"
+              style={{ width: 300 }}
+            >
               <Space direction={'vertical'}>
                 <Space direction={'horizontal'}>
-                  <div style={{fontFamily: 'monospace', fontSize: '80px'}}>R: </div>
-                  <div style={{fontFamily: 'monospace', paddingTop: '30px', fontSize: '25px'}}>{this.state.readRequest}</div>
+                  <div style={{ fontFamily: 'monospace', fontSize: '80px' }}>
+                    R:{' '}
+                  </div>
+                  <div
+                    style={{
+                      fontFamily: 'monospace',
+                      paddingTop: '30px',
+                      fontSize: '25px',
+                    }}
+                  >
+                    {this.state.readRequest}
+                  </div>
                 </Space>
-
               </Space>
             </Card>
           </Col>
-          <Col span={1}/>
+          <Col span={1} />
           <Col span={6}>
-            <Card loading={this.state.loading} title="Total Add" style={{ width: 300 }}>
+            <Card
+              loading={this.state.loading}
+              title="Total Write"
+              style={{ width: 300 }}
+            >
               <Space direction={'horizontal'}>
-                <div style={{fontFamily: 'monospace', fontSize: '80px'}}>W: </div>
-                <div style={{fontFamily: 'monospace',paddingTop: '30px', fontSize: '25px'}}>{this.state.writeRequest}</div>
+                <div style={{ fontFamily: 'monospace', fontSize: '80px' }}>
+                  W:{' '}
+                </div>
+                <div
+                  style={{
+                    fontFamily: 'monospace',
+                    paddingTop: '30px',
+                    fontSize: '25px',
+                  }}
+                >
+                  {this.state.writeRequest}
+                </div>
               </Space>
             </Card>
           </Col>
-          <Col span={2}/>
+          <Col span={2} />
         </Row>
       </div>
-    )
+    );
   }
 }
